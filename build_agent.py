2e344f9c0f0988cc99d0b945b4032bc47eb01b0e
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-06-01 07:44:53 -0500

    Fix to use python3

--- build_agent.py
+++ build_agent.py
@@ -1,8 +1,14 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
 import os
 from jinja2 import Environment, FileSystemLoader
+from pathlib import Path
 
 env = Environment(loader=FileSystemLoader('templates'))
 templates = os.listdir('templates')
+
+Path('output').mkdir(parents=True, exist_ok=True)
 
 for t in templates:
   template = env.get_template(t)
