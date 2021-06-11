1cf337fe0466891d70a99d17e6210acabcbc7a74
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-06-11 16:04:39 -0500

    Parse args

--- build_agent.py
+++ build_agent.py
@@ -1,15 +1,37 @@
 #!/usr/bin/env python3
 # coding=utf-8
 
-import os
+import os, sys, getopt
 from jinja2 import Environment, FileSystemLoader
 from pathlib import Path
 
-env = Environment(loader=FileSystemLoader('templates'))
-templates = os.listdir('templates')
+def main(argv):
+   agent_name = ''
+   output_folder = 'output'
+   try:
+      opts, args = getopt.getopt(argv,"hn:o:",["name=","outputfolder="])
+   except getopt.GetoptError:
+      print 'build_agent.py -n <agentname> -o <outputfolder>'
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print 'build_agent.py -n <agentname> -o <outputfolder>'
+         sys.exit()
+     elif opt in ("-n", "--name"):
+         agent_name = arg
+     elif opt in ("-o", "--outputfolder"):
+         output_folder = arg
+   print 'Agent name: "', agent_name
+   print 'Output folder is "', output_folder
 
-Path('output').mkdir(parents=True, exist_ok=True)
+   env = Environment(loader=FileSystemLoader('templates'))
+   templates = os.listdir('templates')
 
-for t in templates:
-  template = env.get_template(t)
-  template.stream(agent_name='foo').dump( 'output/' + t[:-3]) # Remove ".j2" from name
+   Path(output_folder).mkdir(parents=True, exist_ok=True)
+
+   for t in templates:
+     template = env.get_template(t)
+     template.stream(agent_name=agent_name).dump( output_folder + '/' + t[:-3]) # Remove ".j2" from name
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
